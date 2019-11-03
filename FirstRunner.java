a3dffd6cf0c6f5b4f1041dd98234672e9f5fc8dd
Kacey Wheeler <57329180+kaceywheeler@users.noreply.github.com>
2019-11-03 14:11:03 -0700

    Update FirstRunner.java

--- FirstRunner.java
+++ FirstRunner.java
@@ -1,19 +1,24 @@
 package ifs2;
-
+//Import scanner
 import java.util.Scanner;
+//Import first class
 import ifs2.First;
 
 public class FirstRunner {
 
 	public static void main(String[] args) {
 		
+		//Create new scanner
 		Scanner keyboard = new Scanner(System.in);
 		
+		//Gets user input for text
 		System.out.println("Enter a string: ");
 		String text = keyboard.next();
 		
+		//Creates new object
 		First ref = new First();
 		
+		//Calls method and prints output
 		System.out.println(ref.Repeat(text));
 		
 
